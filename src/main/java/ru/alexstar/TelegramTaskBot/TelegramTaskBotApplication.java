b17e5ddb5f260b4c5bc7fad405e577ce25a002d9
Star46Alex <116678386+Star46Alex@users.noreply.github.com>
2023-03-02 20:01:48 +0300

    Add files via upload
    Spring initilizer

---
+++ src/main/java/ru/alexstar/TelegramTaskBot/TelegramTaskBotApplication.java
@@ -0,0 +1,13 @@
+package ru.alexstar.TelegramTaskBot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TelegramTaskBotApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TelegramTaskBotApplication.class, args);
+	}
+
+}
